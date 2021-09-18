d2dd21d726244c6ba099e5956401d41e5e6b9dd9
向延琳 <xiangyanlin@supermonkey.com.cn>
2021-09-18 16:51:24 +0800

    first commit

---
+++ src/main/java/com/example/xylfast/XylFastApplication.java
@@ -0,0 +1,13 @@
+package com.example.xylfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XylFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XylFastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xylfast/XylFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xylfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XylFastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
